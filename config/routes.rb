6d86cf0707524266bea1d8ec9737b5e0c5b90d0b
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-10-22 17:12:32 +0900

    contactページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/about'
 
+  get 'static_pages/contact'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
