5cc75893c5eb70b382d2dc6fd46ae676d53b93f4
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-10-22 18:27:27 +0900

    routesをnamespaceでの実装に変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace 'static_pages' do
+    get :home
+    get :help
+    get :about
+    get :contact
+  end
+  # get 'static_pages/home'
+  #
+  # get 'static_pages/help'
+  #
+  # get 'static_pages/about'
+  #
+  # get 'static_pages/contact'
+  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
