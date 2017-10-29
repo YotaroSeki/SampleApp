f746a1b37eb2befbf3fd1ea93f29ecd806905be9
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-10-29 18:56:50 +0900

    ”を’に変更してもいいところは全て変更した

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,36 +3,36 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    @base_title = "Ruby on rails tutorial Sample App"
+    @base_title = 'Ruby on rails tutorial Sample App'
   end
 
-  test "should get root" do
+  test 'should get root' do
     get root_url
     assert_response :success
   end
 
-  test "should get home" do
+  test 'should get home' do
     get root_path
     assert_response :success
-    assert_select "title", "#{@base_title}"
+    assert_select 'title', '#{@base_title}'
   end
 
-  test "should get help" do
+  test 'should get help' do
     get help_path
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select 'title', 'Help | #{@base_title}'
   end
 
-  test "should_get_about" do
+  test 'should_get_about' do
     get about_path
     assert response, :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select 'title', 'About | #{@base_title}'
   end
 
-  test "should_get_contact" do
+  test 'should_get_contact' do
     get contact_path
     assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
+    assert_select 'title', 'Contact | #{@base_title}'
   end
 
 end
