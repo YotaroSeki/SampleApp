0560a6b1768aa10b7776f32fdaf22f7510bb3223
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-10-30 19:13:37 +0900

    パスワードの存在性と最低長を保証した

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :password, length:{ minimum: 6 }, presence: true
 
   has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -62,4 +62,14 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_equal mixed_case_address.downcase, @user.reload.email
   end
+
+  test 'password should not be blank' do
+    @user.password = @user.password_confirmation = ' ' * 6
+    assert_not @user.valid?
+  end
+
+  test 'password should have a min length' do
+    @user.password = @user.password_confirmation = 'a' * 5
+    assert_not @user.valid?
+  end
 end
