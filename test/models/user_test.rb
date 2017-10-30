a5a405dee212626456236405aab9afad9a2aaf53
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-10-30 12:50:11 +0900

    Userモデルに対するtestを作成した

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,22 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @user = User.new(name: 'Koji Tadokoro', email: 'yajusenp@i')
+  end
+
+  test 'should be vaild' do
+    assert @user.valid?
+  end
+
+  test 'name should be present' do
+    @user.name = ' '
+    assert_not @user.valid?
+  end
+
+  test 'email should be present' do
+    @user.email = '     '
+    assert_not @user.valid?
+  end
 end
