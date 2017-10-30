28128cecd18752eaf9f05517d82a1d788095dc72
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-10-30 16:29:31 +0900

    データベースに保存する前に小文字にする処理を行うようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+  before_save { self.email = email.downcase }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :name, presence: true, length: { maximum: 50 }
