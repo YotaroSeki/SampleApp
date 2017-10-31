dcd6ec0e550997afe2efdaf434e2eaf7f262ab1b
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-10-31 13:41:15 +0900

    nameとemailにNOT NULL制約を追加した

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171030093714) do
+ActiveRecord::Schema.define(version: 20171031043030) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+    t.string "name", null: false
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
