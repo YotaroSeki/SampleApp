e20f38af5659ebd1ca231dd6b444e30f81f72bd1
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-11-07 16:21:40 +0900

    userのcontrollerにcreateを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,14 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(params[:user])
+
+    if @user.save
+
+    else
+      render 'new'
+    end
+  end
 end
