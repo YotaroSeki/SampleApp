831014f99b712fe7dac89ce19f6e88c81561fb60
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-11-11 18:52:40 +0900

    createアクションでstrongParametersを使うようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
 
     if @user.save
 
@@ -16,4 +16,9 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
