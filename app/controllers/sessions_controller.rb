660b2c5bfebea7ba46d6d0bfd45af7c2ade06380
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-11-12 16:38:49 +0900

    loginできるようにした

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-
+      log_in(user)
+      redirect_to user
     else
       flash.now[:danger] = 'アドレスとパスワードの組み合わせが正しくありません'
       render('new')
