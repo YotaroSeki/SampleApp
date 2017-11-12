df0022b77fe479603729df8c5a50e531a04e4922
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-11-12 18:53:16 +0900

    コメントされたところ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,7 @@ class User < ApplicationRecord
   has_secure_password
 
   def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-               BCrypt::Engine.cost
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 end
