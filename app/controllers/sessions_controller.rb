1b2b1c1958e4666c82af02cef2a92f6bf05b2f4c
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-11-12 15:45:28 +0900

    ユーザーをsessionのemailからさがして検証できるようにした

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+
+    else
+      render('new')
+    end
+  end
+
+  def destroy
+
+  end
 end
