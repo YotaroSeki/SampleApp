28f775c52fe40b14a0495a565cb62ebd4b1ef8b4
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-11-17 17:06:08 +0900

    チェックボックスが動作しているかのテストを追加

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -41,4 +41,12 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select('a[href=?]', logout_path,      count: 0)
     assert_select('a[href=?]', user_path(@user), count: 0)
   end
+
+  test 'login with remembering then login without remembering' do
+    login_as(@user)
+    assert_not_empty(cookies['remember_token'])
+    delete logout_path
+    login_as(@user, remember_me: '0')
+    assert_empty(cookies['remember_token'])
+  end
 end
