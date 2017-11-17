5bc221eb5686b6bfbea104bbef9835c5a3477265
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-11-17 17:05:49 +0900

    変数名を間違えていたのを修正

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,14 +16,14 @@ class ActiveSupport::TestCase
     !session[:user_id].nil?
   end
 
-  def log_in_as(user)
+  def login_as(user)
     session[:user_id] = user.id
   end
 end
 
 class ActionDispatch::IntegrationTest
   # login as test user
-  def log_in(user, password: 'password', remember_me: '1')
+  def login_as(user, password: 'password', remember_me: '1')
     post login_path, params: { session: { email: user.email, password: password, remember_me: remember_me } }
   end
 end
