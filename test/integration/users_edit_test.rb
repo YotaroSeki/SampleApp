04773861e5607f85a3dd13722a6e04b83ea12679
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-11-19 16:10:20 +0900

    成功時に対するtestを作成

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -12,4 +12,17 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template('users/edit')
     assert_select('div.alert', /4 errors/)
   end
+
+  test 'successful edit' do
+    get edit_user_path(@user)
+    assert_template('users/edit')
+    name = 'meu'
+    email = 'memememeumeu@example.com'
+    patch user_path(@user), params:{ user: {name: name, email: email, password: '', password_confirmation: '' } }
+    assert_not(flash.empty?)
+    assert_redirected_to(@user)
+    @user.reload
+    assert_equal(name, @user.name)
+    assert_equal(email, @user.email)
+  end
 end
