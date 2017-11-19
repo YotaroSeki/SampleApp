42c596d088777e92038721ec68340a0e79d5007c
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-11-19 16:46:08 +0900

    他人のプロフィールを変更しようとしたらrootに飛ぶようになるとGREENなtestを書いた

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:meumeu)
+    @other_user = users(:rinrin)
   end
 
   test 'should get new' do
@@ -20,6 +21,20 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     patch user_path(@user), params: { user: { name: @user.name, email: @user.email } }
     assert_not_empty(flash)
     assert_redirected_to(login_url)
+  end
+
+  test 'should redirect edit when logged in as wrong user' do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert_not_empty(flash)
+    assert_redirected_to(root_url)
+  end
+
+  test 'should redirect update when logged in as wrong user' do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { name: @user.name, email: @user.email } }
+    assert_not_empty(flash)
+    assert_redirected_to(root_url)
   end
 
 end
