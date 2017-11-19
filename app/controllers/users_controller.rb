6b7228d49dc516a45904fa3522c0d495ea1ca8bf
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-11-19 20:53:21 +0900

    destroyに対するadmin属性追加後のtestを書いた

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -68,6 +68,6 @@ class UsersController < ApplicationController
   end
 
   def admin_user
-    redirect_to root_url unless correct_user.admin?
+    redirect_to root_url unless current_user.admin?
   end
 end
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,15 +3,29 @@ require 'test_helper'
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:meumeu)
+    @non_admin = users(:ibubu)
   end
 
-  test 'index includes pagination' do
+  test 'index as admin including pagination and delete links' do
     login_as(@user)
     get users_path
     assert_template('users/index')
     assert_select('div.pagination', count: 2)
-    User.paginate(page: 1).each do |user|
+    first_page = User.paginate(page: 1)
+    first_page.each do |user|
       assert_select('a[href=?]', user_path(user), text: user.name)
+      unless (user == @user)
+        assert_select('a[href=?]', user_path(user), text: 'delete')
+      end
     end
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
+    end
+  end
+
+  test 'index as non admin' do
+    login_as(@non_admin)
+    get users_path
+    assert_select('a', text: 'delete', count: 0)
   end
 end
