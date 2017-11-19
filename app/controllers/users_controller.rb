23dc1820005ed42afdfd2e81b634bd52b8e9e52e
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-11-19 17:07:16 +0900

    アクセス先とログインユーザーが正しいかを調べるようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -38,7 +40,7 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user)
-          .permit(:name, :email, :password, :password_confirmation)
+        .permit(:name, :email, :password, :password_confirmation)
   end
 
   def logged_in_user
@@ -46,5 +48,10 @@ class UsersController < ApplicationController
       flash[:danger] = 'Please login'
       redirect_to login_url
     end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
