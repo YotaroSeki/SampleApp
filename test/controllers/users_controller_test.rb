cbbdcfa2c0a89dddf49497f906eb05d1fabde92a
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-11-19 20:27:27 +0900

    destroyに対するtestを作成

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -42,4 +42,26 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to(login_url)
   end
 
+  test 'should not allow the admin attribute to be edited via the web' do
+    login_as(@other_user)
+    assert_not(@other_user.admin?)
+    patch user_path(@other_user), params: {user: { password: 'popopo', password_confirmation: 'popopo', admin: true } }
+    assert_not(@other_user.reload.admin?)
+  end
+
+  test 'should redirect destroy when not logged in' do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to(login_url)
+  end
+
+  test 'should redirect destroy when logged in as a non-admin' do
+    login_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
+
 end
