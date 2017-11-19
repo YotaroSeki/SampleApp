ccd9f0c6d20a854046c32889c5d2eaa687ac1a6a
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-11-19 17:24:00 +0900

    前コミットへの追加もれ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,6 +45,7 @@ class UsersController < ApplicationController
 
   def logged_in_user
     unless (logged_in?)
+      store_location
       flash[:danger] = 'Please login'
       redirect_to login_url
     end
