850c8da8d0e0b5e018f586599fddbdf27c90b336
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-11-19 17:52:46 +0900

    loginしていないユーザーはユーザー一覧を見られないべきだというtestを作成

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -37,4 +37,9 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to(root_url)
   end
 
+  test 'should redirect index when not logged in' do
+    get users_path
+    assert_redirected(login_url)
+  end
+
 end
