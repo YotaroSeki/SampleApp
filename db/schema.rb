21883fb0264ea1920a10c3016d0167268197786e
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-11-25 15:43:35 +0900

    micropostのモデルを作った

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_171_119_110_144) do
+ActiveRecord::Schema.define(version: 20_171_124_100_846) do
+  create_table 'microposts', force: :cascade do |t|
+    t.text 'content'
+    t.integer 'user_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index %w[user_id created_at], name: 'index_microposts_on_user_id_and_created_at'
+    t.index ['user_id'], name: 'index_microposts_on_user_id'
+  end
+
   create_table 'users', force: :cascade do |t|
     t.string 'name', null: false
     t.string 'email', null: false
