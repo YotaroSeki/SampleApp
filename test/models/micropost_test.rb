ec6cccf9bca7ca3323bb94234c895e637ced7041
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-11-25 16:02:11 +0900

    micropostに対するテストを作成

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:meumeu)
+    @micropost = Micropost.new(content: 'Test', user_id: @user.id)
+  end
+
+  test 'should be valid' do
+    assert(@micropost.valid?)
+  end
+
+  test 'user id should be present' do
+    @micropost.user_id = nil
+    assert_not(@micropost.valid?)
+  end
 end
