19e29cba1753d484a66ce04356dedea11af8eb61
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-11-25 16:37:34 +0900

    userが複数のmicropostを持つことを明示した

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :microposts
   attr_accessor :remember_token
-  before_save { self.email = email.downcase }
+  before_save {self.email = email.downcase}
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }, presence: true, allow_nil: true
-
-  has_secure_password
 
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
