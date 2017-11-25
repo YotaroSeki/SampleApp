5e96a916f0c84d5d843fb08e264fd3466246339b
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-11-26 02:40:23 +0900

    @micropostを#homeで作成

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,14 @@
 class StaticPagesController < ApplicationController
-  def home; end
+  def home
+    @micropost = current_user.microposts.build if logged_in?
+  end
 
-  def help; end
+  def help;
+  end
 
-  def about; end
+  def about;
+  end
 
-  def contact; end
+  def contact;
+  end
 end
