5c36da5740c4d52da1a39f9472ddb53c370a36f3
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-11-26 14:24:20 +0900

    pictureをdbに追加した

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_171_124_100_846) do
+ActiveRecord::Schema.define(version: 20_171_126_051_902) do
   create_table 'microposts', force: :cascade do |t|
     t.text 'content'
     t.integer 'user_id'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.string 'picture'
     t.index %w[user_id created_at], name: 'index_microposts_on_user_id_and_created_at'
     t.index ['user_id'], name: 'index_microposts_on_user_id'
   end
