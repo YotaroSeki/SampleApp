c1f3adc4abb0fb7bc0e27abe4bf5924fdf6fa2ff
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-11-26 19:15:53 +0900

    フォロー数、フォロワー数を表示できるようにした パーシャルの名前を変更した

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,11 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :microposts, only: %i[create destroy]
 
   # namespace 'static_pages' do
