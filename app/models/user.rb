f20cd82ac9a3dcab03d54e5d9c9e68a05d07621e
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-11-26 18:03:11 +0900

    ユーザーはfollow、followedを多数持っているようにした(active_relationshipsという名前はクソ！)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
   attr_accessor :remember_token
   before_save { self.email = email.downcase }
 
