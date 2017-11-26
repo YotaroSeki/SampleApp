6df0751e0f22ef43af81fc4a50c7a35cfdaa6f63
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-11-26 18:16:02 +0900

    relationshipに対するテストを作成し、通るようにした

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class RelationshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @relationship = Relationship.new(follower_id: users(:meumeu).id, followed_id: users(:ibubu).id)
+  end
+
+  test 'should be valid' do
+    assert(@relationship.valid?)
+  end
+
+  test 'should require a follower id' do
+    @relationship.follower_id = nil
+    assert_not(@relationship.valid?)
+  end
+
+  test 'should require a followed id' do
+    @relationship.followed_id = nil
+    assert_not(@relationship.valid?)
+  end
 end
