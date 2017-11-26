6adb0c3e190775be111ccef0471d050ff6f3333c
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-11-26 20:15:13 +0900

    followingアクションとfollowersアクション及びそれに対応するviewを定義した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: %i[edit update index destroy]
+  before_action :logged_in_user, only: %i[edit update index destroy following followers]
   before_action :correct_user, only: %i[edit update]
   before_action :admin_user, only: :destroy
 
@@ -48,11 +48,25 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
+  def following
+    @title = 'Following'
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = 'Followers'
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
   private
 
   def user_params
     params.require(:user)
-          .permit(:name, :email, :password, :password_confirmation)
+        .permit(:name, :email, :password, :password_confirmation)
   end
 
   def correct_user
