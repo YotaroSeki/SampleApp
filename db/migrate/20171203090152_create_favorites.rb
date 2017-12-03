fbe4ee6ec202da7e79d24ac8e382010a2bd3be50
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-12-03 18:12:57 +0900

    null:falseした

--- db/migrate/20171203090152_create_favorites.rb
+++ db/migrate/20171203090152_create_favorites.rb
@@ -1,8 +1,8 @@
 class CreateFavorites < ActiveRecord::Migration[5.1]
   def change
     create_table :favorites do |t|
-      t.references :user, foreign_key: true
-      t.references :micropost, foreign_key: true
+      t.references :user, foreign_key: true, null: false
+      t.references :micropost, foreign_key: true, null: false
 
       t.timestamps
     end
