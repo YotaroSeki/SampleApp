9d2582b62278de73fd7653e8250487f2ad6e46b1
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-12-03 18:45:47 +0900

    バリデーションの追加 favoriteモデルのテストの作成 favorites.ymlを一時的にコメントアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships
+  has_many :favorites, dependent: :destroy
   attr_accessor :remember_token
   before_save { self.email = email.downcase }
 
--- test/models/favorite_test.rb
+++ test/models/favorite_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class FavoriteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @favorite = Favorite.new(user_id: users(:meumeu).id, micropost_id: microposts(:ants).id)
+  end
+
+  test 'should be valid' do
+    assert(@favorite.valid?)
+  end
+
+  test 'should require user_id' do
+    @favorite.user_id = nil
+    assert_not(@favorite.valid?)
+  end
+
+  test 'should require micropost_id' do
+    @favorite.micropost_id = nil
+    assert_not(@favorite.valid?)
+  end
 end
