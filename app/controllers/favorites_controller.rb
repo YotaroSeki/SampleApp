bd0df6491ebcd7c9b56aa4f00491a83595f27092
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2017-12-08 17:00:10 +0900

    Ajaxを用いて非同期でふぁぼれるようにした

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,20 @@
 class FavoritesController < ApplicationController
   def create
-    micropost = Micropost.find_by(id: params[:micropost_id])
-    current_user.favorite(micropost)
-    redirect_to current_user
+    @micropost = Micropost.find_by(id: params[:micropost_id])
+    current_user.favorite(@micropost)
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_path) }
+      format.js
+    end
   end
 
   def destroy
-    micropost = Favorite.find_by(micropost_id: params[:id]).micropost
-    current_user.cancel_favorite(micropost)
-    redirect_to current_user
+    @micropost = Favorite.find_by(id: params[:id]).micropost
+    puts @micropost.content
+    current_user.cancel_favorite(@micropost)
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_path) }
+      format.js
+    end
   end
 end
