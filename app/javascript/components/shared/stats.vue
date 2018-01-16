257742f05910a02f9b3a375cb9998b453ced4e7c
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2018-01-16 16:16:34 +0900

    link->path

--- app/javascript/components/shared/stats.vue
+++ app/javascript/components/shared/stats.vue
@@ -1,18 +1,18 @@
 <template>
     <div class='stats'>
-        <a v-if='stats.following' :href='stats.following.link'>
+        <a v-if='stats.following' :href='stats.following.path'>
             <strong id='following' class='stat'>
                 {{stats.following.count}}
             </strong>
             following
         </a>
-        <a v-if='stats.followers' :href='stats.followers.link'>
+        <a v-if='stats.followers' :href='stats.followers.path'>
             <strong id='followers' class='stat'>
                 {{stats.followers.count}}
             </strong>
             followers
         </a>
-        <a v-if='stats.favorites' :href='stats.favorites.link'>
+        <a v-if='stats.favorites' :href='stats.favorites.path'>
             <strong id='favorites' class='stat'>
                 {{stats.favorites.count}}
             </strong>
