cd5d879ac13c5215dc7ea927e5a72a5bfa16814e
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2018-01-18 19:06:45 +0900

    変数名の変更忘れを修正

--- app/javascript/components/shared/user_info.vue
+++ app/javascript/components/shared/user_info.vue
@@ -1,7 +1,7 @@
 <template>
     <section class='user_info'>
         <img :src='profile.user_icon'/>
-        <h1>{{profile.user.name}}</h1>
+        <h1>{{profile.current_user.name}}</h1>
         <span>
             <a :href='profile.user_path'>view my profile</a>
         </span>
