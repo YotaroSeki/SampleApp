92d5c0d40cbabf544ba98f215ad2b098942e9e45
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2018-01-27 15:26:56 +0900

    gravatarのurl指定をhelperに切り出した

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,4 +5,8 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: 'gravatar')
   end
+
+  def gravatar_url(user, size: 80)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email.downcase)}?s=#{size}"
+  end
 end
