4115ac0bfb420c187b329695ece9c4fd7fc678f2
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2018-02-17 17:50:43 +0900

    お気に入りの取り消しがFavoriteFormになってて名前がやばかったので修正した

--- app/javascript/components/favorites/FavoriteCancelForm.vue
+++ app/javascript/components/favorites/FavoriteCancelForm.vue
@@ -1,31 +1,30 @@
 <template>
     <div>
-        <button
-        @click='favorite'>
-            favorite
+        <button @click=cancel_favorite>
+        cancel favorite
         </button>
     </div>
 </template>
 
 <script type='text/javascript'>
 export default {
-    name: 'FavoriteForm',
+    name: 'FavoriteCancelForm',
     props: {
         micropost: Object
     },
     methods: {
-        favorite() {
-            this.axios.post(this.micropost.favorite_path)
+        cancel_favorite() {
+            this.axios.delete(this.micropost.favorite_path)
                 .then((res) => {
                     console.log(res);
                     this.$notify({
                         title: 'Success',
-                        message: 'favorite succeed!',
+                        message: 'favorite canceled!',
                         type: 'success'
                     });
-                    this.$emit('favorite-succeed', this.micropost.id)
+                    this.$emit('favorite-canceled', this.micropost.id)
                 })
-                .catch((error) => {
+                .catch((error)  => {
                     console.log(error);
                 });
         }
