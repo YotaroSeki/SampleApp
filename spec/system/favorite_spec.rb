d0d966e932923e36ac3c82f96686dc95855fa802
Yotaro Seki <youyoutarou.dog0115@gmail.com>
2018-03-09 13:14:05 +0900

    要求される動作をtestしそれがGreenになることを確認した

--- spec/system/favorite_spec.rb
+++ spec/system/favorite_spec.rb
@@ -5,22 +5,36 @@ feature 'Favorites', type: :system do
   let(:other_user) { create(:other_user) }
 
   background do
-    login_user.follow(other_user)
-    other_user.follow(login_user)
-    other_user.microposts.map do |micropost|
+    other_user.microposts.each do |micropost|
       login_user.favorite(micropost)
     end
+    @favorite_count_at_the_start = Favorite.count.freeze
+    login(login_user)
   end
 
   scenario 'cancel posting favorites and make it favorite again', js: true do
-    login(login_user)
-    # visit favorites_user_path(login_user)
-    #
-    # expect(page).to have_button 'cancel favorite'
-    sleep 5
-    visit 'users/1/favorites'
-    sleep 10
-    visit 'users/1/favorites.jsonld'
-    sleep 10
+    visit favorites_user_path(login_user)
+
+    expect(page).to have_button 'cancel favorite'
+    expect(page).not_to have_css '.favorite-form'
+    expect(all('.favorite-cancel-form').size).to eq @favorite_count_at_the_start
+
+    first('.favorite-cancel-form').click_button 'cancel favorite'
+    expect(page).to have_button 'favorite'
+    expect(page).to have_content 'favorite canceled!'
+    expect(Favorite.count).to eq @favorite_count_at_the_start - 1
+    expect(all('.favorite-cancel-form').size).to eq @favorite_count_at_the_start - 1
+    expect(all('.favorite-form').size).to eq 1
+
+    click_button 'favorite'
+    expect(page).not_to have_css '.favorite-form'
+    expect(page).to have_content 'favorite succeed!'
+    expect(Favorite.count).to eq @favorite_count_at_the_start
+    expect(all('.favorite-cancel-form').size).to eq @favorite_count_at_the_start
+
+  end
+
+  after do
+    page.driver.quit
   end
 end
